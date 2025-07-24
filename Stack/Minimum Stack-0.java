eec5b4eb8dcce9439ad00b5afee8d577c8c9aa72
Antriksh <antrikshjan@gmail.com>
2025-07-25 02:19:40 +0530

    Minimum Stack-0

--- Stack/Minimum Stack-0.java
+++ Stack/Minimum Stack-0.java
@@ -94,7 +94,7 @@ public class Main{
             if(inp.equals("display")){
                 ns.display();
             } else if(inp.contains("push")){
-                int val = Integer.parseInt(inp.replaceAll("[A-Za-z\\s]", ""));
+                int val = Integer.parseInt(inp.replaceAll("[A-Za-z\\]", ""));
                 ns.push(val);
             } else if(inp.equals("top")){
                 System.out.println(ns.top());
