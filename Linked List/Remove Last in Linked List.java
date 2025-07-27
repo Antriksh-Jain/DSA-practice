4afc3f4254c9f966677dab0e83f9adb6085017e7
Antriksh <antrikshjan@gmail.com>
2025-07-27 23:43:42 +0530

    Remove Last in Linked List

--- Linked List/Remove Last in Linked List.java
+++ Linked List/Remove Last in Linked List.java
@@ -122,8 +122,12 @@ public class Main{
         }
         
         void removeLast(){
-            if(size==0 || size == 1){
-                removeFirst();
+            if(size==0){
+                System.out.println("List is empty");
+            } else if(size == 1){
+                head = null;
+                tail = null;
+                size = 0;
             } else {
                 Node temp = head;
                 for(int i = 0; i<size-2; i++){
