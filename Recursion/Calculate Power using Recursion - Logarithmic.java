b3d9022ae3479098601fd24c07c75aa3125a5598
Antriksh <antrikshjan@gmail.com>
2025-07-30 00:46:09 +0530

    Calculate Power using Recursion - Logarithmic

---
+++ Recursion/Calculate Power using Recursion - Logarithmic.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        System.out.println(power(x, n));
+    }
+    
+    static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        int xnb2 = power(x, n/2);
+        int xn = xnb2 * xnb2;
+        
+        if(n % 2 == 1){
+            xn *= x;
+        }
+        return xn;
+    }
+}
