d23ff3f08abbadaf7c119a7596cbbba7a3e8cddc
Antriksh <antrikshjan@gmail.com>
2025-07-30 01:17:10 +0530

    Print Zig-Zag

---
+++ Recursion/Print Zig-Zag.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+    
+    static void pzz(int n){
+        if(n==0){
+            return;
+        }
+        
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+    }
+}
