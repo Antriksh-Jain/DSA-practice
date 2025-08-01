0556b0d0742b899f09990a766da08d1a976fdc97
Antriksh <antrikshjan@gmail.com>
2025-08-02 01:54:18 +0530

    Print Permutations

---
+++ Recursion/Print Permutations.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printPermutations(str, "");
+    }
+    
+    static void printPermutations(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i+1);
+            
+            printPermutations(ros, ans + ch);
+        }
+    }
+}
