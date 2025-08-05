717282d396fb02a918e5869df2fccc0055b13c11
Antriksh <antrikshjan@gmail.com>
2025-08-06 00:18:42 +0530

    Two Stack in an Array

---
+++ Stack/Two Stack in an Array.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+public class Main{
+    public static class TwoStack {
+        int[] data;
+        int tos1;
+        int tos2;
+        
+        public TwoStack(int cap){
+            data = new int[cap];
+            tos1 = -1;
+            tos2 = data.length;
+        }
+        
+        int size1(){
+            return tos1 + 1;
+        }
+        
+        int size2(){
+            return data.length - tos2;
+        }
+        
+        void push1(int val){
+            if(tos2 == tos1 + 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos1++;
+                data[tos1] = val;
+            }
+        }
+        
+        void push2(int val){
+            if(tos2 == tos1 + 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos2--;
+                data[tos2] = val;
+            }
+        }
+        
+        int pop1(){
+            if(size1() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos1];
+                tos1--;
+                return val;
+            }
+        }
+        
+        int pop2(){
+            if(size2() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos2];
+                tos2++;
+                return val;
+            }
+        }
+        
+        int top1(){
+            if(size1() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos1];
+                return val;
+            }
+        }
+        
+        int top2(){
+            if(size2() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos2];
+                return val;
+            }
+        }
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.nextLine();
+        
+        TwoStack ts = new TwoStack(n);
+        
+        String str = scn.nextLine();
+        while(str.equals("exit")==false){
+            if(str.equals("size1")){
+                System.out.println(ts.size1());
+            } else if(str.startsWith("push1")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                ts.push1(val);
+            } else if(str.equals("pop1")){
+                System.out.println(ts.pop1());
+            } else if(str.equals("top1")){
+                System.out.println(ts.top1());
+            } else if(str.equals("size2")){
+                System.out.println(ts.size2());
+            } else if(str.startsWith("push2")){
+                int val = Integer.parseInt(str.split(" ")[1]);
+                ts.push2(val);
+            } else if(str.equals("pop2")){
+                System.out.println(ts.pop2());
+            } else if(str.equals("top2")){
+                System.out.println(ts.top2());
+            }
+            
+            str = scn.nextLine();
+        }
+    }
+}
