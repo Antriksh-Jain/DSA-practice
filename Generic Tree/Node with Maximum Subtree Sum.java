321ddace83a5c890638d104fd2b46344848b112f
Antriksh <antrikshjan@gmail.com>
2025-08-13 23:04:42 +0530

    Node with Maximum Subtree Sum

---
+++ Generic Tree/Node with Maximum Subtree Sum.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Main{
+    private static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+    
+    public static void display(Node node){
+        String str = node.data + " -> ";
+        for(Node child: node.children){
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+        
+        for(Node child: node.children){
+            display(child);
+        }
+    }
+    
+    public static Node construct(int[] arr){
+        Node root = null;
+        
+        Stack<Node> st = new Stack<>();
+        
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == -1){
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+                
+                if(st.size() > 0){
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+                
+                st.push(t);
+            }
+        }
+        
+        return root;
+    }
+    
+    static int msn;
+    static int ms;
+    public static int nodeWithMSST(Node node){
+        int sum = 0;
+        msn = 0;
+        ms = Integer.MIN_VALUE;
+        
+        for(Node child: node.children){
+            int cs = nodeWithMSST(child);
+            sum += cs;
+        }
+        
+        sum += node.data;
+        
+        if(sum > ms){
+            ms = sum;
+            msn = node.data;
+        }
+        
+        return sum;
+    }
+    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.nextLine();
+        int[] arr = new int[n];
+        String str = scn.nextLine();
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(str.split(" ")[i]);
+        }
+        
+        Node root = construct(arr);
+        
+        nodeWithMSST(root);
+        System.out.println(msn + "@" + ms);
+    }
+}
