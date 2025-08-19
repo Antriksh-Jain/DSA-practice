4896688cbac3579d4d094cdc0dd056a4d360393f
Antriksh <antrikshjan@gmail.com>
2025-08-19 23:15:56 +0530

    Rotate by 90 degrees

---
+++ Arrays/Rotate by 90 degrees.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
+        scn.nextLine();
+        for(int i = 0; i<arr.length; i++){
+            String str = scn.nextLine();
+            for(int j = 0; j<arr[0].length; j++){
+                arr[i][j] = Integer.parseInt(str.split(" ")[j]);
+            }
+        }
+        
+        //transpose
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i; j<arr[0].length; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        
+        //reverse column
+        for(int i = 0; i<arr.length; i++){
+            int li = 0;
+            int ri = arr[0].length - 1;
+            
+            while(li < ri){
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                
+                li++;
+                ri--;
+            }
+        }
+        
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
