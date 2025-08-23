c94de22993b2468bbd553ee7988b9551d2dc0b5b
Antriksh <antrikshjan@gmail.com>
2025-08-23 23:50:19 +0530

    Constructor of BST

---
+++ Binary Search Tree/Constructor of BST.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public static Node construct(int[] arr, int l, int h){
+        if(l > h){
+            return null;
+        }
+        
+        int mid = (l+h)/2;
+        int data = arr[mid];
+        
+        Node lc = construct(arr, l, mid - 1);
+        Node rc = construct(arr, mid + 1, h);
+        
+        Node node = new Node(data, lc, rc);
+        
+        return node;
+    }
+    
+    public static void display(Node node){
+        if(node == null){
+            return;
+        } else {
+            String str = "";
+            str += node.left == null? "." : node.left.data + "";
+            str += " <- " + node.data + " -> ";
+            str += node.right == null? "." : node.right.data + "";
+            System.out.println(str);
+            
+            display(node.left);
+            display(node.right);
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        
+        int[] arr = new int[str.split(" ").length];
+        
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(str.split(" ")[i]);
+        }
+        
+        Node root = construct(arr, 0, arr.length-1);
+        
+        display(root);
+    }
+}
