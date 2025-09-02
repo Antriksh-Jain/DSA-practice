f5fdc675c9806a7c6e17a52b3568aae551b96554
Antriksh <antrikshjan@gmail.com>
2025-09-02 22:23:05 +0530

    Paint Fence

---
+++ Dynamic Programming/Paint Fence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+        
+        int same = k;
+        int diff = k * (k - 1);
+        int total = same + diff;
+        
+        for(int i = 3; i <= n; i++){
+            same = diff;
+            diff = total * (k - 1);
+            total = same + diff;
+        }
+        
+        System.out.println(total);
+    }
+}
