f50e6f35cfab6f2efaac8a8e33a04454aaae641c
Antriksh <antrikshjan@gmail.com>
2025-09-06 22:23:09 +0530

    Sort 01

---
+++ Time and Space/Sort 01.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main{
+    public static void sort01(int[] arr){
+        int i = 0;
+        int j = 0;
+        
+        while(i < arr.length){
+            if(arr[i] == 1){
+                i++;
+            } else if(arr[i] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                
+                i++;
+                j++;
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        sort01(arr);
+        
+        for(int val: arr){
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
