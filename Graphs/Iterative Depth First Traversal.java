659e44b5f0eb891e6f04c40f92766c7dbb4991a5
Antriksh <antrikshjan@gmail.com>
2025-09-14 01:57:51 +0530

    Iterative Depth First Traversal

---
+++ Graphs/Iterative Depth First Traversal.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main{
+  public static class Edge{
+    int src;
+    int nbr;
+    
+    Edge(int src, int nbr){
+      this.src = src;
+      this.nbr = nbr;
+    }
+  }
+  
+  public static class Pair{
+    int v;
+    String psf;
+    
+    Pair(int v, String psf){
+      this.v = v;
+      this.psf = psf;
+    }
+  }
+  
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    
+    int vtces = scn.nextInt();
+    int edges = scn.nextInt();
+    
+    @SuppressWarnings("unchecked")
+    ArrayList<Edge>[] graph = new ArrayList[vtces];
+    
+    for(int v = 0; v < vtces; v++){
+      graph[v] = new ArrayList<>();
+    }
+    
+    for(int e = 0; e < edges; e++){
+      int v1 = scn.nextInt();
+      int v2 = scn.nextInt();
+      
+      graph[v1].add(new Edge(v1, v2));
+      graph[v2].add(new Edge(v2, v1));
+    }
+    
+    int src = scn.nextInt();
+    
+    Stack<Pair> st = new Stack<>();
+    boolean[] visited = new boolean[vtces];
+    st.push(new Pair(src, src + ""));
+    
+    while(st.size() > 0){
+      Pair rem = st.pop();
+      
+      if(visited[rem.v] == true){
+        continue;
+      } else {
+        visited[rem.v] = true;
+      }
+      
+      System.out.println(rem.v + "@" + rem.psf);
+      
+      for(Edge e: graph[rem.v]){
+        if(visited[e.nbr] == false){
+          st.push(new Pair(e.nbr, rem.psf + e.nbr));
+        }
+      }
+    }
+  }
+}
