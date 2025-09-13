3d4d95e3fef3cf54fbadf2730bb5dfe644fd6d43
Antriksh <antrikshjan@gmail.com>
2025-09-14 01:08:16 +0530

    Dijkstra Algorithm in Graphs

---
+++ Graphs/Dijkstra Algorithm in Graphs.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Main{
+  public static class Edge{
+    int src;
+    int nbr;
+    int wt;
+    
+    Edge(int src, int nbr, int wt){
+      this.src = src;
+      this.nbr = nbr;
+      this.wt = wt;
+    }
+  }
+  
+  public static class Pair implements Comparable<Pair>{
+    int v;
+    String psf;
+    int wsf;
+    
+    Pair(int v, String psf, int wsf){
+      this.v = v;
+      this.psf = psf;
+      this.wsf = wsf;
+    }
+    
+    public int compareTo(Pair o){
+      return this.wsf - o.wsf;
+    }
+  }
+  
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    
+    int vtces = scn.nextInt();
+    int edges = scn.nextInt();
+    
+    @SuppressWarnings("unchecked")
+    ArrayList<Edge>[] graph = new ArrayList[vtces];
+    
+    for(int v = 0; v < vtces; v++){
+      graph[v] = new ArrayList<>();
+    }
+    
+    for(int e = 0; e < edges; e++){
+      int v1 = scn.nextInt();
+      int v2 = scn.nextInt();
+      int wt = scn.nextInt();
+      
+      graph[v1].add(new Edge(v1, v2, wt));
+      graph[v2].add(new Edge(v2, v1, wt));
+    }
+    
+    int src = scn.nextInt();
+    
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+    pq.add(new Pair(src, src + "", 0));
+    boolean[] visited = new boolean[vtces];
+    
+    while(pq.size() > 0){
+      Pair rem = pq.remove();
+      
+      if(visited[rem.v] == true){
+        continue;
+      } else {
+        visited[rem.v] = true;
+      }
+      
+      System.out.println(rem.v + " via " + rem.psf + " @ " + rem.wsf);
+      
+      for(Edge e: graph[rem.v]){
+        if(visited[e.nbr] == false){
+          pq.add(new Pair(e.nbr, rem.psf + e.nbr, rem.wsf + e.wt));
+        }
+      }
+    }
+  }
+}
